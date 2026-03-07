1cb88b374c36bbf673affa849cf90271052f2bc4
PeachyKeen12 <vlaz1999@yahoo.com>
2026-03-07 12:46:52 -0500

    Update streamlit_app.py: Added requests block

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,13 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
     
         st.success('Your Smoothie is ordered!', icon="✅")
+
+# -- new section to display smoothlyfront nutrition information
+# --
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
     
 # --------------------------------------------------------------------
 
